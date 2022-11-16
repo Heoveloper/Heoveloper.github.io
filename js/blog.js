3cd3a209da4217ed3fda75f0e8cf51c32156cf9c
Junhyeok Heo <wnsgur5508@gmail.com>
2022-11-16 23:46:23 +0900

    new blog layout

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,18 @@
+//선택자
 //home
 const $home = document.querySelector(".wrapper");
 //about contents
 const $about = document.querySelector(".about");
 //skills contents
 const $skills = document.querySelector(".skills");
+//project contents
+const $project = document.querySelector(".project");
+//education contents
+const $education = document.querySelector(".education");
+//contact contents
+const $contact = document.querySelector(".contact");
 
+//메뉴 클릭 시 해당 콘텐츠로 스크롤 이동
 gnb_home.addEventListener('click', e => {
     e.preventDefault();
     $home.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
@@ -33,4 +41,29 @@ gnb_skills.addEventListener('click', e => {
 menu_skills.addEventListener('click', e => {
     e.preventDefault();
     $skills.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
+})
+
+gnb_project.addEventListener('click', e => {
+    e.preventDefault();
+    $project.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
+})
+
+menu_project.addEventListener('click', e => {
+    e.preventDefault();
+    $project.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
+})
+
+menu_education.addEventListener('click', e => {
+    e.preventDefault();
+    $education.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
+})
+
+gnb_contact.addEventListener('click', e => {
+    e.preventDefault();
+    $contact.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
+})
+
+menu_contact.addEventListener('click', e => {
+    e.preventDefault();
+    $contact.scrollIntoView({behavior:"smooth", block:"start", inline:"start"});
 })
